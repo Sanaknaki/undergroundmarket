dd6295d4d0ee9c6e661cf3cb7a6d7c2d85d888db
Ali Sanaknaki <asanaknaki@shoplogix.com>
2021-01-26 11:29:31 -0500

    Redirect URI changed to live.

--- src/utils/Credentials.js
+++ src/utils/Credentials.js
@@ -1,7 +1,7 @@
 export const Credentials = () => {
     const client_id = "d7a4392f350d493090f11dea6a95f1ce"; // Your client id
     const client_secret = "f55d28528eeb4e55a385f4dedb7b502b"; // Your secret
-    const redirect_uri = "http://localhost:3000/"; // Your redirect uri
+    const redirect_uri = "https://sanaknaki.github.io/undergroundmarket/"; // Your redirect uri
     const scopes = [
         "user-read-email",
         "user-read-currently-playing",
